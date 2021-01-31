41de2be78dfeacb495e7b7a500e40ce1e34e723f
lipenodias <lipenodias@gmail.com>
2021-01-31 12:20:18 -0300

    ok

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,7 @@ data "aws_ami" "debian" {
 resource "aws_instance" "k8s" {
    
     ami           = data.aws_ami.debian.id
-    for_each      = [ for i in var.tipo_recurso : { index = i } if i < 7 ]
+    for_each      = { for i, b in var.tipo_recurso : b => { index = i } if i < 7 }
     instance_type = each.key
     key_name      = "chave_aws"
     tags          = {  
