6f332c9e3bb642845ce33ef1c68b84ec8e034a47
lipenodias <lipenodias@gmail.com>
2021-01-31 13:06:57 -0300

    Alterado codigo terraform para  Cluster HA k8s

--- variable.tf
+++ variable.tf
@@ -1,17 +1,5 @@
-variable "contador" {
-    type = string
-    default = 1
-    description = "Contagem de instancias a criar"
-}
-
 variable "nome_no" {
     type        = list(string)
     default     = []
     description = "Nome dos nós do k8s"
-}
-
-variable "tipo_recurso" {
-    type        = list(string)
-    default     = ["t2.micro","t2.medium"]
-    description = "List of instance type"
 }
